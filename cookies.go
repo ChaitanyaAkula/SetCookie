3abe31cfacd89283d5946552c5dbd8ba5231fb02
ChaitanyaAkula <krishchaituakula@gmail.com>
2020-06-30 00:41:53 +0530

    Update cookies.go

--- cookies.go
+++ cookies.go
@@ -1,12 +1,15 @@
 package cookies
-import "net/http"
-
-
+import (
+	"net/http"
+	"time"
+)
+var m,_=time.ParseDuration("325600m")
+var expire=time.Now().Add(m)
 func SetCookie(w http.ResponseWriter,name,value string){
         http.SetCookie(w,&http.Cookie{
 		Name:name,
 		Value:value,
-	//	Expires:expire,
+		Expires:expire, // persistent cookie , max age = 1 year
 		HttpOnly: true,
 	})
 
